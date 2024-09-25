96974bb3afeb10434b862543ac774c7e81ca801e
paroliav <viv.github@gmail.com>
2024-09-24 23:37:08 -0400

    Update crypto.js

--- api/server/utils/crypto.js
+++ api/server/utils/crypto.js
@@ -1,45 +1,23 @@
-require('dotenv').config();
-
 const crypto = require('crypto');
-const key = Buffer.from(process.env.CREDS_KEY, 'hex');
-const iv = Buffer.from(process.env.CREDS_IV, 'hex');
-const algorithm = 'aes-256-cbc';
 
-function encrypt(value) {
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
-  let encrypted = cipher.update(value, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  return encrypted;
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY || crypto.randomBytes(32).toString('hex');
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
-function decrypt(encryptedValue) {
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
-  let decrypted = decipher.update(encryptedValue, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return decrypted;
+function decrypt(text) {
+  const textParts = text.split(':');
+  const iv = Buffer.from(textParts.shift(), 'hex');
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY, 'hex'), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
 }
 
-// Programatically generate iv
-function encryptV2(value) {
-  const gen_iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(algorithm, key, gen_iv);
-  let encrypted = cipher.update(value, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  return gen_iv.toString('hex') + ':' + encrypted;
-}
-
-function decryptV2(encryptedValue) {
-  const parts = encryptedValue.split(':');
-  // Already decrypted from an earlier invocation
-  if (parts.length === 1) {
-    return parts[0];
-  }
-  const gen_iv = Buffer.from(parts.shift(), 'hex');
-  const encrypted = parts.join(':');
-  const decipher = crypto.createDecipheriv(algorithm, key, gen_iv);
-  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return decrypted;
-}
-
-module.exports = { encrypt, decrypt, encryptV2, decryptV2 };
+module.exports = { encrypt, decrypt };
